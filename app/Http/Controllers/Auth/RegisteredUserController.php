8e4cf783b87017ce55221f4110746cd8d1aeb544
Brandon <nbr4nd0n2005@gmail.com>
2025-10-25 17:23:06 +0700

    Made some changes to redirecting

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,6 +46,8 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        // CHANGE THIS LINE
+        // return redirect(route('dashboard', absolute: false)); // Original
+        return redirect(route('issues.index', absolute: false)); // New redirect
     }
 }
