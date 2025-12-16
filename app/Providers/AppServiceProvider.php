0971ee6b1432e11481a0930728553fa0cee5059c
Brandon's Laptop <BrandonsLaptop@Nicholass-MacBook-Air.local>
2025-12-16 09:48:40 +0700

    modified code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
         }
         Vite::prefetch(concurrency: 3);
+        Schema::defaultStringLength(191);
     }
 }
